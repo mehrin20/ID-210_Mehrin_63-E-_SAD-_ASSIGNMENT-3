0202fe32574b4195f1a448306ba2f07e7ac45111
Mehrin <mehrinchowdhury20@gmail.com>
2026-04-26 02:23:17 +0600

    Update

--- flutter_2_mehrin/flutter_mehrin_2/lib/.homepage.dart
+++ flutter_2_mehrin/flutter_mehrin_2/lib/.homepage.dart
@@ -6,9 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink.shade50,
+      backgroundColor: Colors.pink.shade100,
       appBar: AppBar(
-        title: Text("Home 🌸"), //Text
+        title: Text("Home "), //Text
         backgroundColor: Colors.pink.shade200,
         foregroundColor: Colors.pink.shade100,
         // leading: Icon(Icons.home, color: Colors.amber),
@@ -17,81 +17,139 @@ class HomePage extends StatelessWidget {
         //   IconButton(onPressed: () {}, icon: Icon(Icons.person)),
         // ],
       ), //AppBar
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              "Leading  University ",
-              style: TextStyle(color: Colors.pink.shade700, fontSize: 90),
-            ),
-          ), //Padding
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              "Batch - 63",
-              style: TextStyle(color: Colors.pink.shade800, fontSize: 70),
-            ),
-          ), //Padding
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Center(
+                child: Text(
+                  "Leading  University ",
+                  style: TextStyle(
+                    color: Colors.pink.shade700,
+                    fontSize: 90,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ), //Padding
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  "Batch - 63",
+                  style: TextStyle(color: Colors.pink.shade800, fontSize: 70),
+                ),
+              ),
+            ), //Padding
+            Container(
+              width: 800,
+              height: 100,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Colors.pink.shade300,
+                border: Border.all(color: Colors.pink.shade900, width: 5),
+                borderRadius: BorderRadius.all(Radius.circular(70)),
+              ),
+              child: Center(
+                child: Text(
+                  "Student Portal ",
+                  style: TextStyle(
+                    color: Colors.pink.shade50,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ), //Text
+              ),
+            ), //Container
+            Container(
+              width: 800,
+              height: 80,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(30),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.pink.shade400,
+                border: Border.all(color: Colors.pink.shade900, width: 5),
+                borderRadius: BorderRadius.all(Radius.circular(70)),
+              ),
+              child: Center(
+                child: Text(
+                  "Sections",
+                  style: TextStyle(
+                    color: Colors.pink.shade50,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ), //Text
+              ),
+            ), //Container
+            Container(
+              width: 800,
+              height: 80,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(30),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.pink.shade400,
+                border: Border.all(color: Colors.pink.shade900, width: 5),
+                borderRadius: BorderRadius.all(Radius.circular(200)),
+              ),
+              child: Center(
+                child: Text(
+                  "Students List",
+                  style: TextStyle(
+                    color: Colors.pink.shade50,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ), //Text
+              ),
+            ), //Container
+            SizedBox(
+              height: 200,
+              width: 600,
 
-          Container(
-            width: 800,
-            height: 100,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(color: Colors.pink.shade200),
-            child: Text(
-              "Student Portal                             >",
-              style: TextStyle(color: Colors.pink.shade50, fontSize: 50),
-            ), //Text
-          ), //Container
-          Container(
-            width: 800,
-            height: 70,
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(color: Colors.pink.shade400),
-            child: Text(
-              "Sections",
-              style: TextStyle(color: Colors.pink.shade50, fontSize: 25),
-            ), //Text
-          ), //Container
-          Container(
-            width: 800,
-            height: 70,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(color: Colors.pink.shade400),
-            child: Text(
-              "Students List",
-              style: TextStyle(color: Colors.pink.shade50, fontSize: 25),
-            ), //Text
-          ), //Container
-        ],
-      ), //Column
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.pink.shade900, width: 5),
+                ),
+                color: Colors.pink.shade300,
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/flutter.png",
+                    height: 200,
+                    width: 180,
+                  ),
+                ),
+              ), //Card
+            ), //SizeBox
+          ],
+        ), //Column
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.pink.shade900,
         foregroundColor: Colors.pink.shade100,
         onPressed: () {},
         child: Icon(Icons.admin_panel_settings_sharp),
       ), //FloatingActionButton
+
       endDrawer: NavigationDrawer(
         backgroundColor: Colors.pink.shade50,
         children: [
           ListTile(
-            //ListTile
             leading: Icon(Icons.home, color: Colors.pink.shade900),
             title: Text("HomePage"),
             onTap: () {},
           ), //ListTile
           ListTile(
-            //ListTile
             leading: Icon(Icons.settings, color: Colors.pink.shade900),
             title: Text("Settings"),
             onTap: () {},
           ), //ListTile
           ListTile(
-            //ListTile
             leading: Icon(Icons.person, color: Colors.pink.shade900),
             title: Text("Profile"),
             onTap: () {},
