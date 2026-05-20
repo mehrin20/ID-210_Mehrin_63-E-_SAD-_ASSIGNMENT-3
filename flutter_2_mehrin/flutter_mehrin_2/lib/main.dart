7e7c23b4e0c20e0665ae6aeb84f9c1453bdac135
Mehrin <mehrinchowdhury20@gmail.com>
2026-05-20 23:28:01 +0600

    Updated project

--- flutter_2_mehrin/flutter_mehrin_2/lib/main.dart
+++ flutter_2_mehrin/flutter_mehrin_2/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_mehrin_2/.homepage.dart';
 import 'package:flutter_mehrin_2/auth_gate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
